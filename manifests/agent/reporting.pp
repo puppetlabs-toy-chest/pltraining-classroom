1dea4bca51b96433522efb532266da57b0c14f8c
Josh Samuelson <js@puppetlabs.com>
2015-04-30 14:25:31 -0700

    Fixing missing o

--- manifests/agent/reporting.pp
+++ manifests/agent/reporting.pp
@@ -2,7 +2,7 @@ class classroom::agent::reporting {
   file { '/usr/local/bin/process_reports.rb':
     ensure  => file,
     mode    => 755,
-    source  => 'puppet:///modules/classrom/process_reports.rb',
+    source  => 'puppet:///modules/classroom/process_reports.rb',
     replace => false,
   }
 }
