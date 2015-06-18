ce640d74f054b32ae6717ae4469bbf31a6ab4c72
Josh Samuelson <js@puppetlabs.com>
2015-06-18 15:46:58 -0700

    Fine tuning docker settings

--- manifests/course/infrastructure.pp
+++ manifests/course/infrastructure.pp
@@ -16,65 +16,38 @@ class classroom::course::infrastructure {
     require    => File['/etc/docker/agent/'],
   }
 
-  docker::run { 'agent1.puppetlabs.vm':
+  Docker::Run {
     image            => 'agent',
     command          => '/sbin/init 3',
+    use_name         => true,
+    privileged       => true,
+    volumes          => ['/var/yum:/var/yum', '/sys/fs/cgroup:/sys/fs/cgroup:ro'],
+    extra_parameters => ["--add-host \"${fqdn} master.puppetlabs.vm puppet:${ipaddress_docker0}\"","--restart=always"],
+    require          => Docker::Image['agent'],
+  }
+  docker::run { 'agent1.puppetlabs.vm':
     hostname         => 'agent1.puppetlabs.vm',
     ports            => ['10080:80'],
-    use_name         => true,
-    volumes          => ['/var/yum:/var/yum', '/sys/fs/cgroup:/sys/fs/cgroup:ro'],
-    extra_parameters => "--add-host \"${fqdn} master.puppetlabs.vm puppet:${ipaddress_docker0}\"",
-    require          => Docker::Image['agent'],
   }
   docker::run { 'agent2.puppetlabs.vm':
-    image            => 'agent',
-    command          => '/sbin/init 3',
     hostname         => 'agent2.puppetlabs.vm',
     ports            => ['20080:80'],
-    use_name         => true,
-    volumes          => ['/var/yum:/var/yum', '/sys/fs/cgroup:/sys/fs/cgroup:ro'],
-    extra_parameters => "--add-host \"${fqdn} master.puppetlabs.vm puppet:${ipaddress_docker0}\"",
-    require          => Docker::Image['agent'],
   }
   docker::run { 'agent3.puppetlabs.vm':
-    image            => 'agent',
-    command          => '/sbin/init 3',
     hostname         => 'agent3.puppetlabs.vm',
     ports            => ['30080:80'],
-    use_name         => true,
-    volumes          => ['/var/yum:/var/yum', '/sys/fs/cgroup:/sys/fs/cgroup:ro'],
-    extra_parameters => "--add-host \"${fqdn} master.puppetlabs.vm puppet:${ipaddress_docker0}\"",
-    require          => Docker::Image['agent'],
   }
   docker::run { 'agent4.puppetlabs.vm':
-    image            => 'agent',
-    command          => '/sbin/init 3',
     hostname         => 'agent4.puppetlabs.vm',
     ports            => ['40080:80'],
-    use_name         => true,
-    volumes          => ['/var/yum:/var/yum', '/sys/fs/cgroup:/sys/fs/cgroup:ro'],
-    extra_parameters => "--add-host \"${fqdn} master.puppetlabs.vm puppet:${ipaddress_docker0}\"",
-    require          => Docker::Image['agent'],
   }
   docker::run { 'agent5.puppetlabs.vm':
-    image            => 'agent',
-    command          => '/sbin/init 3',
     hostname         => 'agent5.puppetlabs.vm',
     ports            => ['50080:80'],
-    use_name         => true,
-    volumes          => ['/var/yum:/var/yum', '/sys/fs/cgroup:/sys/fs/cgroup:ro'],
-    extra_parameters => "--add-host \"${fqdn} master.puppetlabs.vm puppet:${ipaddress_docker0}\"",
-    require          => Docker::Image['agent'],
   }
   docker::run { 'agent6.puppetlabs.vm':
-    image            => 'agent',
-    command          => '/sbin/init 3',
     hostname         => 'agent6.puppetlabs.vm',
     ports            => ['60080:80'],
-    use_name         => true,
-    volumes          => ['/var/yum:/var/yum', '/sys/fs/cgroup:/sys/fs/cgroup:ro'],
-    extra_parameters => "--add-host \"${fqdn} master.puppetlabs.vm puppet:${ipaddress_docker0}\"",
-    require          => Docker::Image['agent'],
   }
 
 }
