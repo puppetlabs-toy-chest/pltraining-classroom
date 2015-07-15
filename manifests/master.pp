e692e68222943ca408305e495c83959120cc1327
Ben Ford <ben.ford@puppetlabs.com>
2015-07-15 16:24:34 -0700

    yay, the init script blocks!

--- manifests/master.pp
+++ manifests/master.pp
@@ -12,27 +12,6 @@ class classroom::master {
   # These files are cached by the build, so this will work offline
   include pe_repo::platform::el_6_i386
 
-  # we know that you all love logging back into the Console every time you do a
-  # demo, but we're sadists, so we're going to take that pleasure away from you.
-  # Newer PE versions are configured via Hiera key
-  if versioncmp($::pe_version, '3.7.0') < 0 {
-    file { '/etc/puppetlabs/console-services/conf.d/rbac-session.conf':
-      ensure => file,
-      source => 'puppet:///modules/classroom/rbac-session.conf',
-      notify => Service['pe-console-services'],
-    }
-  }
-
-  # https://docs.puppetlabs.com/puppet/latest/reference/environments_configuring.html#environmenttimeout
-  # Suggests that this setting can be pushed up to puppet.conf globally.
-  # Initial testing appears to confirm that. If this proves problematic, then
-  # uncomment this resource and the relevant resource in classroom::agent::workdir
-  # file { "/etc/puppetlabs/puppet/environments/production/environment.conf":
-  #   ensure  => file,
-  #   content => "environment_timeout = 0\n",
-  #   replace => false,
-  # }
-
   # Ensure the environment cache is disabled and restart if needed
   ini_setting {'environment timeout':
     ensure  => present,
@@ -68,12 +47,6 @@ class classroom::master {
 
   # Now create all of the users who've checked in
   Classroom::User <<||>>
-
-  # The new PE stack takes a very long time to startup, which can cause
-  # disconcerting errors. This simply schedules that to the end of the run
-  # and waits for the service to resume servicing requests before allowing
-  # the run to complete
-  include classroom::master::wait_for_startup
 
   # Add autoteam yaml
   include classroom::master::autoteam
