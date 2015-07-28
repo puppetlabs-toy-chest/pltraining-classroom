95d4838fd2ef686a2c0f1ddedac239403ef580f9
Carthik Sharma <carthik@puppetlabs.com>
2015-07-28 09:15:48 -0700

    Only try to do these things if you're dealing with a puppet master
    In the event of someone classifying all nodes in the infrastructure
    course with the classroom::course::infrastructure class, we need to
    ensure that we don't try to run containers within containers.
    
    Also fixed the indentation for existing code.

--- manifests/course/infrastructure.pp
+++ manifests/course/infrastructure.pp
@@ -1,74 +1,78 @@
 # This Class sets up the docker environment and containers for
 # the Infrastructure course
 #
-class classroom::course::infrastructure {
-  include docker
+class classroom::course::infrastructure (
+  $role      = $classroom::params::role,
+) inherits classroom::params {
+  
+  if $role == 'master' {
+    include docker
 
-  file { '/etc/docker/agent/':
-    ensure  => directory,
-    recurse => true,
-    source  => 'puppet:///modules/classroom/dockeragent/',
-    require => Class['docker'],
-  }
-  file { '/etc/docker/ssl_dir/':
-    ensure => directory,
-  }
-  file { '/usr/local/bin/run_agents':
-    ensure => present,
-    mode   => '755',
-    source => 'puppet:///modules/classroom/run_agents.sh',
-  }
+    file { '/etc/docker/agent/':
+      ensure  => directory,
+      recurse => true,
+      source  => 'puppet:///modules/classroom/dockeragent/',
+      require => Class['docker'],
+    }
+    file { '/etc/docker/ssl_dir/':
+      ensure => directory,
+    }
+    file { '/usr/local/bin/run_agents':
+      ensure => present,
+      mode   => '755',
+      source => 'puppet:///modules/classroom/run_agents.sh',
+    }
 
-  docker::image {'agent':
-    docker_dir => '/etc/docker/agent/',
-    require    => File['/etc/docker/agent/'],
-  }
+    docker::image {'agent':
+      docker_dir => '/etc/docker/agent/',
+      require    => File['/etc/docker/agent/'],
+    }
 
-  if $::ipaddress_docker0 { 
-  Docker::Run {
-    image            => 'agent',
-    command          => '/sbin/init 3',
-    use_name         => true,
-    privileged       => true,
-    volumes          => [
-      '/var/yum:/var/yum',
-      '/sys/fs/cgroup:/sys/fs/cgroup:ro',
-      '/etc/docker/ssl_dir/:/etc/puppetlabs/puppet/ssl'
-    ],
-    extra_parameters => [
-      "--add-host \"${::fqdn} master.puppetlabs.vm puppetfactory puppet:${::ipaddress_docker0}\"",
-      "--restart=always"
-    ],
-    require          => [
-      Docker::Image['agent'],
-      File['/etc/docker/ssl_dir/']
-    ],
-  }
+    if $::ipaddress_docker0 { 
+      Docker::Run {
+        image            => 'agent',
+        command          => '/sbin/init 3',
+        use_name         => true,
+        privileged       => true,
+        volumes          => [
+          '/var/yum:/var/yum',
+          '/sys/fs/cgroup:/sys/fs/cgroup:ro',
+          '/etc/docker/ssl_dir/:/etc/puppetlabs/puppet/ssl'
+        ],
+        extra_parameters => [
+          "--add-host \"${::fqdn} master.puppetlabs.vm puppetfactory puppet:${::ipaddress_docker0}\"",
+          "--restart=always"
+        ],
+        require          => [
+          Docker::Image['agent'],
+          File['/etc/docker/ssl_dir/']
+        ],
+      }
 
-  docker::run { 'test.puppetlabs.vm':
-    hostname => 'test.puppetlabs.vm',
-    ports    => ['10080:80'],
-  }
-  docker::run { 'web1.puppetlabs.vm':
-    hostname => 'web1.puppetlabs.vm',
-    ports    => ['20080:80'],
-  }
-  docker::run { 'db2dev.puppetlabs.vm':
-    hostname => 'db2dev.puppetlabs.vm',
-    ports    => ['30080:80'],
-  }
-  docker::run { 'web2dev.puppetlabs.vm':
-    hostname => 'web2dev.puppetlabs.vm',
-    ports    => ['40080:80'],
-  }
+      docker::run { 'test.puppetlabs.vm':
+        hostname => 'test.puppetlabs.vm',
+        ports    => ['10080:80'],
+      }
+      docker::run { 'web1.puppetlabs.vm':
+        hostname => 'web1.puppetlabs.vm',
+        ports    => ['20080:80'],
+      }
+      docker::run { 'db2dev.puppetlabs.vm':
+        hostname => 'db2dev.puppetlabs.vm',
+        ports    => ['30080:80'],
+      }
+      docker::run { 'web2dev.puppetlabs.vm':
+        hostname => 'web2dev.puppetlabs.vm',
+        ports    => ['40080:80'],
+      }
 
-  # For dummy containers that stop immediately after running puppet:
-  #docker::run {'dummy1.puppetlabs.vm':
-  #  command  => 'puppet agent -t',
-  #  hostname => 'dummy1.puppetlabs.vm',
-  #}
+      # For dummy containers that stop immediately after running puppet:
+      #docker::run {'dummy1.puppetlabs.vm':
+      #  command  => 'puppet agent -t',
+      #  hostname => 'dummy1.puppetlabs.vm',
+      #}
 
-} else {
-  notice('ipaddress_docker0 is not yet defined, rerun puppet to configure docker containers')
-}
+    } else {
+    notice('ipaddress_docker0 is not yet defined, rerun puppet to configure docker containers')
+  }
 }
