c1232f9637d67e066e4880ea4543b6f9c897085d
Ben Ford <ben.ford@puppetlabs.com>
2015-08-31 09:15:51 -0700

    Don't try to manage cacert on windows
    TRAINTECH-206 #ready

--- manifests/cacert.pp
+++ manifests/cacert.pp
@@ -1,15 +1,20 @@
 class classroom::cacert {
   assert_private('This class should not be called directly')
 
-  file { '/etc/pki/ca-trust/source/anchors/classroom.crt':
-    ensure => file,
-    source => "${classroom::confdir}/ssl/certs/ca.pem",
-    notify => Exec['trust classroom ca'],
-  }
+  if $::osfamily != 'windows' {
+    file { '/etc/pki/ca-trust/source/anchors/classroom.crt':
+      ensure => file,
+      source => "${classroom::confdir}/ssl/certs/ca.pem",
+      notify => Exec['trust classroom ca'],
+    }
 
-  exec { 'trust classroom ca':
-    command     => '/usr/bin/update-ca-trust extract',
-    onlyif      => '/usr/bin/update-ca-trust enable',
-    refreshonly => true,
+    exec { 'trust classroom ca':
+      command     => '/usr/bin/update-ca-trust extract',
+      onlyif      => '/usr/bin/update-ca-trust enable',
+      refreshonly => true,
+    }
+  }
+  else {
+    # We don't need this for windows yet.
   }
 }
