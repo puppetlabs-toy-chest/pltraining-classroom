eb6974da904144c70b6892cde1d9466a0b20171d
Josh Samuelson <js@puppetlabs.com>
2015-09-14 12:05:43 -0700

    Grafan in agent class

---
+++ manifests/agent/logging.pp
@@ -0,0 +1,80 @@
+class classroom::agent::logging {
+
+  # Configure Graphite & Grafana
+  include '::apache'
+
+  file { '/opt/graphite':
+    ensure => 'directory',
+  }
+
+  apache::vhost { $::fqdn:
+    servername      => $::fqdn,
+    port            => 8081,
+    docroot         => '/opt/grafana',
+    error_log_file  => 'grafana_error.log',
+    access_log_file => 'grafana_access.log',
+    directories     => [
+      {
+        path            => '/opt/grafana',
+        options         => [ 'None' ],
+        allow           => 'from All',
+        allow_override  => [ 'None' ],
+        order           => 'Allow,Deny',
+      }
+    ]
+  }->
+  class {'grafana':
+    graphite_host      => $::ipaddress,
+    elasticsearch_host => $::fqdn,
+    elasticsearch_port => 9200,
+  }
+
+  apache::vhost { $::ipaddress:
+    port    => '80',
+    docroot => '/opt/graphite/webapp',
+    wsgi_application_group      => '%{GLOBAL}',
+    wsgi_daemon_process         => 'graphite',
+    wsgi_daemon_process_options => {
+      processes          => '5',
+      threads            => '5',
+      display-name       => '%{GROUP}',
+      inactivity-timeout => '120',
+    },
+    wsgi_import_script          => '/opt/graphite/conf/graphite.wsgi',
+    wsgi_import_script_options  => {
+      process-group     => 'graphite',
+      application-group => '%{GLOBAL}'
+    },
+    wsgi_process_group          => 'graphite',
+    wsgi_script_aliases         => {
+      '/' => '/opt/graphite/conf/graphite.wsgi'
+    },
+    headers => [
+      'set Access-Control-Allow-Origin "*"',
+      'set Access-Control-Allow-Methods "GET, OPTIONS, POST"',
+      'set Access-Control-Allow-Headers "origin, authorization, accept"',
+    ],
+    directories => [{
+      path => '/media/',
+      order => 'deny,allow',
+      allow => 'from all'}
+    ]
+  }->
+  class { 'graphite':
+    gr_web_server           => 'none',
+    gr_disable_webapp_cache => true,
+    gr_storage_schemas      => [
+    {
+      name       => 'carbon',
+      pattern    => '^carbon\.',
+      retentions => '1m:90d'
+    },
+    {
+      name       => 'default',
+      pattern    => '.*',
+      retentions => '1m:30m,1m:1d,5m:2y'
+    },
+  ],
+
+  }
+}
