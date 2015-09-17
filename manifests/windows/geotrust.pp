5a693dbba11fd5bd45ce14f8de73808d9719e8b5
Gabriel M Schuyler <gabe@puppetlabs.com>
2015-09-17 13:36:45 -0700

    Replace Invoke-WebRequest with longer, but more compatible command

--- manifests/windows/geotrust.pp
+++ manifests/windows/geotrust.pp
@@ -2,13 +2,13 @@ class classroom::windows::geotrust {
   assert_private('This class should not be called directly')
 
   exec { 'download-geotrust-cert':
-    command  => 'Invoke-Webrequest https://www.geotrust.com/resources/root_certificates/certificates/GeoTrust_Global_CA.pem -outfile c:\windows\temp\GeoTrust_Glocal_CA.pem',
-    creates  => 'c:/windows/temp/GeoTrust_Glocal_CA.pem',
+    command  => '$wc = New-Object System.Net.WebClient;$wc.DownloadFile("https://www.geotrust.com/resources/root_certificates/certificates/GeoTrust_Global_CA.pem","C:\Windows\Temp\GeoTrust_Glocal_CA.pem")',
+    creates  => 'C:/Windows/Temp/GeoTrust_Glocal_CA.pem',
     provider => powershell,
     timeout  => $classroom::timeout,
   }
   exec { 'install-geotrust-cert':
-    command     => 'certutil -addstore root c:\windows\temp\GeoTrust_Glocal_CA.pem',
+    command     => 'certutil -addstore root C:\Windows\Temp\GeoTrust_Glocal_CA.pem',
     provider    => powershell,
     refreshonly => true,
     subscribe   => Exec['download-geotrust-cert'],
