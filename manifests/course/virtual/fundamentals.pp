367ff5553e7ee23297db42ed4b4671b34b76656a
Ben Ford <ben.ford@puppetlabs.com>
2016-02-04 13:51:54 -0800

    Update fundamentals profile

--- manifests/course/virtual/fundamentals.pp
+++ manifests/course/virtual/fundamentals.pp
@@ -2,28 +2,43 @@ class classroom::course::virtual::fundamentals (
   $session_id = $classroom::params::session_id,
 ) inherits classroom::params {
 
-  showoff::presentation { 'fundamentals':
-    path    => "${showoff::root}/courseware/fundamentals",
-    require => Class['classroom::master::courseware'],
+  File {
+    owner => 'root',
+    group => 'root',
+    mode  => '0644',
   }
 
-  ensure_packages('gcc', {
+  class { 'classroom::master::showoff':
+    password => $session_id,
+  }
+
+  ensure_packages(['gcc', 'zlib-devel'], {
     before => Package['puppetfactory']
   })
+
+  package { ['serverspec', 'puppetlabs_spec_helper']:
+    ensure   => present,
+    provider => gem,
+    require  => Package['puppet'],
+  }
+
+  # lol, this is great.
+  package { 'puppet':
+    ensure          => present,
+    provider        => gem,
+    install_options => { '--bindir' => '/tmp' },
+  }
 
   class { 'puppetfactory':
     dashboard        => true,
     prefix           => true,
     map_environments => true,
     map_modulepath   => false,
+    dashboard        => "${showoff::root}/courseware/_files/tests",
     session_id       => $session_id,
+    gitlab_enabled   => false,
   }
 
-  File {
-    owner => 'root',
-    group => 'root',
-    mode  => '0644',
-  }
 
   file { '/etc/puppetlabs/r10k/r10k.yaml':
     ensure  => file,
@@ -44,7 +59,7 @@ class classroom::course::virtual::fundamentals (
   file { '/etc/puppetfactory/hooks/create/r10k_create_user.rb':
     ensure => file,
     mode   => '0755',
-    source => 'puppet:///modules/puppetfactory/fundamentals/r10k_env.rb',
+    content => epp('puppetfactory/fundamentals/r10k_env.rb.epp',{ 'gitserver' => $puppetfactory::gitserver }),
   }
 
   # this looks wonky, but the script uses its name to determine mode of operation
@@ -67,7 +82,7 @@ class classroom::course::virtual::fundamentals (
     require => Class['r10k::webhook::config'],
   }
 
-  class { 'class::facts':
+  class { 'classroom::facts':
     coursename => 'fundamentals',
   }
 
