f0335fc41bd7a32b51a8e2ec9b96c2bd28d981ed
Josh Samuelson <js@puppetlabs.com>
2016-02-04 13:29:30 -0800

    Use showff on AMD

--- manifests/course/appropriate_module.pp
+++ manifests/course/appropriate_module.pp
@@ -1,12 +1,18 @@
 class classroom::course::virtual::appropriate_module (
   $session_id = $classroom::params::session_id,
+  $role = $classroom::params::role,
 ) inherits classroom::params {
-  # Classroom for Appropriate Module Design course
-  class { 'puppetfactory':
-    # Put students' puppetcode directories somewhere obvious
-    puppetcode       => '/root/puppetcode',
-    map_environments => true,
-    container_name   => 'centosagent',
-    session_id       => $session_id,
+  if $role == 'master' { 
+    # Classroom for Appropriate Module Design course
+    class { 'puppetfactory':
+      # Put students' puppetcode directories somewhere obvious
+      puppetcode       => '/root/puppetcode',
+      map_environments => true,
+      container_name   => 'centosagent',
+      session_id       => $session_id,
+    }
+    class {'classroom::master::showoff':
+      password => $session_id,
+    }
   }
 }
