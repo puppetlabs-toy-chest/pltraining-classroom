e439dd38fd2c4cb952e6af15d5e1500f82eefef0
Josh Samuelson <js@puppetlabs.com>
2016-04-13 13:11:16 -0700

    TRAINTECH-336 #resolved #time 1h

--- manifests/master/windows.pp
+++ manifests/master/windows.pp
@@ -1,6 +1,11 @@
 # Make sure the windows setup files are on the master for the students
 # to download and configure the Windows Server for the installation of
 # the puppet agent.
+
+# Works similarly to curl | bash solution for linux with the following commands in powershell
+# [System.Net.ServicePointManager]::ServerCertificateValidationCallback={$true}
+# iex (New-Object System.Net.WebClient).DownloadString('https://ipaddress:8140/packages/current/setup_windows.ps1')
+
 class classroom::master::windows {
   require pe_repo::platform::windows_x86_64
   assert_private('This class should not be called directly')
