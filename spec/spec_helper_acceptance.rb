e4b431ba7d81516af861ee621ec16582677a7165
Josh Samuelson <js@puppetlabs.com>
2016-06-27 11:14:33 -0700

    Add modulepath for testing classroom module

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,4 +1,4 @@
 require 'beaker-rspec'
 
 on default, puppet('module', 'install', 'pltraining-classroom', '--modulepath=/etc/puppetlabs/code/modules')
-copy_module_to(default, source: '.', module_name: 'classroom')
+copy_module_to(default, source: '.', module_name: 'classroom', target_module_path: '/etc/puppetlabs/code/modules')
