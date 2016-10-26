dc19f592945457ef9b5161f1f4cb4311fe51caba
Ben Ford <ben.ford@puppetlabs.com>
2016-10-26 10:34:12 -0700

    WIP rubygems bs

--- manifests/windows/rubygems_update.pp
+++ manifests/windows/rubygems_update.pp
@@ -2,17 +2,17 @@ class classroom::windows::rubygems_update {
   assert_private('This class should not be called directly')
 
   Exec {
-    path => 'C:\Program Files\Puppet Labs\Puppet\sys\ruby\bin',
+    path => 'c:/windows/sysnative;c:/windows/system32;C:/Program Files/Puppet Labs/Puppet/sys/ruby/bin',
   }
 
   # yes, this makes me sad too
   unless versioncmp($::rubygems_version, '2.6.7') >= 0 {
-    exec { 'gem sources -r https://rubygems.org/': }
-    -> exec { 'gem sources -a http://rubygems.org/': }
+    exec { 'cmd.exe /c gem sources -r https://rubygems.org/': }
+    -> exec { 'cmd.exe /c gem sources -a http://rubygems.org/': } # doesn't work yet, it prompts for confirmation
 
-    -> exec { 'gem update --system': }
+    -> exec { 'cmd.exe /c gem update --system': }
 
-    -> exec { 'gem sources -r http://rubygems.org/': }
-    -> exec { 'gem sources -a https://rubygems.org/': }
+    -> exec { 'cmd.exe /c gem sources -r http://rubygems.org/': }
+    -> exec { 'cmd.exe /c gem sources -a https://rubygems.org/': }
   }
 }
