10db587c18d2f9dae702b28aeba3a4a01db558f9
Ben Ford <ben.ford@puppetlabs.com>
2016-10-26 12:09:10 -0700

    Updating properly just ain't working
    In the meantime, we'll just disable HTTPS.

--- manifests/windows/rubygems_update.pp
+++ manifests/windows/rubygems_update.pp
@@ -1,18 +1,21 @@
 class classroom::windows::rubygems_update {
   assert_private('This class should not be called directly')
 
-  Exec {
-    path => 'C:\Program Files\Puppet Labs\Puppet\sys\ruby\bin',
-  }
-
   # yes, this makes me sad too
   unless versioncmp($::rubygems_version, '2.6.7') >= 0 {
-    exec { 'gem sources -r https://rubygems.org/': }
-    -> exec { 'gem sources -a http://rubygems.org/': }
+    file { 'C:/Users/Administrator/.gemrc':
+      owner  => 'Administrator',
+      group  => 'Administrators',
+      mode   => '0644',
+      source => 'puppet:///modules/classroom/windows/gemrc',
+    }
 
-    -> exec { 'gem update --system': }
+    File['C:/Users/Administrator/.gemrc'] -> Package<| provider == gem |>
 
-    -> exec { 'gem sources -r http://rubygems.org/': }
-    -> exec { 'gem sources -a https://rubygems.org/': }
+# the rubygems update doesn't seem to work. Let's just disable https completely for now.
+#     exec { 'cmd.exe /c gem update --system':
+#       path    => 'c:/windows/sysnative;c:/windows/system32;C:/Program Files/Puppet Labs/Puppet/sys/ruby/bin',
+#       require => File['C:/Users/Administrator/.gemrc'],
+#     }
   }
 }
