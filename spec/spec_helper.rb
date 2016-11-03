5e97f6837a6616edd86ea91b3d1520ac1ad9f69f
Josh Samuelson <js@puppet.com>
2016-11-03 10:21:45 -0700

    Remove unneeded code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,12 +22,6 @@ RSpec.configure do |c|
       }
     }
   }
-  c.module_path     = File.join(File.dirname(File.expand_path(__FILE__)), 'fixtures', 'modules')
-  c.manifest_dir    = File.join(File.dirname(File.expand_path(__FILE__)), 'fixtures', 'manifests')
-  c.manifest        = File.join(File.dirname(File.expand_path(__FILE__)), 'fixtures', 'manifests', 'site.pp')
-  c.environmentpath = File.join(Dir.pwd, 'spec')
-  c.hiera_config    = File.join(File.dirname(File.expand_path(__FILE__)), 'fixtures', 'hiera.yaml')
-
   c.after(:suite) do
     RSpec::Puppet::Coverage.report!
   end
