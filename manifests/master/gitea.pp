51bd66d15c81d0729228e066d0a10b39e5c174b8
kjhenner <kjhenner@gmail.com>
2016-11-18 11:10:38 -0800

    gitea ini template should be set with content parameter

--- manifests/master/gitea.pp
+++ manifests/master/gitea.pp
@@ -7,12 +7,12 @@ class classroom::master::gitea {
   }
 
   file { '/home/git/go/bin/custom/conf/app.ini':
-    ensure => file,
-    owner  => 'git',
-    group  => 'git',
-    mode   => '0644',
-    source => 'puppet:///modules/classroom/app.ini.erb',
-    notify => Service['gitea'],
+    ensure  => file,
+    owner   => 'git',
+    group   => 'git',
+    mode    => '0644',
+    content => template('classroom/app.ini.erb'),
+    notify  => Service['gitea'],
   }
 
   service { 'gitea':
