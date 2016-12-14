e14352a7a767204a9a722b5203236a40db5852bc
Paul Anderson <paul.anderson@puppet.com>
2016-12-14 04:13:05 -0500

    Better to adjust basemodulepath than copy files

--- manifests/agent/cache.pp
+++ manifests/agent/cache.pp
@@ -1,4 +1,4 @@
-# Ensures that all offline agents have required modules cached
+# Ensures that all offline agents have access to cached modules
 #
 # Warning: Do not use in production - this is a hack specifically for
 # puppetlabs training courses
@@ -7,20 +7,29 @@
 #   Classify all agent nodes
 #
 class classroom::agent::cache (
-  $cachedir = '/usr/src/forge',
-  $moduledir = "${::classroom::params::codedir}/modules",
-) inherits classroom::params {
+
+  #This variable should configure Puppet to use the local cache.
+  $offline_bmp_line = 'basemodulepath = /etc/puppetlabs/code/modules:/opt/puppetlabs/puppet/modules:/usr/src/forge'
+
+) {
   assert_private('This class should not be called directly')
 
-  if $::kernel =~ /Linux/ and $::classroom::offline {
-    exec { 'Copy cached modules for offline use' :
-      command   => "rsync -aP  ${cachedir}/ ${moduledir}/",
-      path      => '/usr/bin/',
-      creates   => "${moduledir}/wsus_client",
-      logoutput => 'on_failure',
-    }
-
+  if $::kernel =~ /Linux/ {
+    if $::classroom::offline {
+      file_line { 'add module cache to basemodulepath' :
+        ensure  => present,
+        path    => '/etc/puppetlabs/puppet/puppet.conf',
+        line    => $offline_bmp_line,
+        after   => '/\[main\]/',
+        match   => '/^\s*basemodulepath\s*=/',
+        replace => true,
+      }
+  } else {
+      file_line { 'remove module cache from basemodulepath' :
+        ensure => absent,
+        path   => '/etc/puppetlabs/puppet/puppet.conf',
+        line   => $offline_bmp_line,
+      }
   }
-
 
 }
