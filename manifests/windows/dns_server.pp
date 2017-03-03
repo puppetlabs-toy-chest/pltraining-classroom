60e974dc204e8d0d8c516d40018c5802780fdf0b
Nekototori <jessykah.bird@gmail.com>
2017-03-03 15:24:20 -0600

    Type default and fixed syntax

--- manifests/windows/dns_server.pp
+++ manifests/windows/dns_server.pp
@@ -1,13 +1,18 @@
 # Set a DNS server on the windows agents
 define classroom::windows::dns_server (
-  $adserver_ip = 'facts[value]{name = "ipaddress" and certname = "adserver"},
+  $adserver_ip_query = 'facts[value]{name = "ipaddress" and certname = "adserver"}',
+  $adserver_ip_lookup = puppetdb_query($adserver_ip_query)
   $ip = '8.8.8.8',
 ) {
+  $adserver_ip = $adserver_ip_lookup ? {
+    Array   => $adserver_ip_lookup[0][value],
+    default => '8.8.4.4',
+    }
     # Only run on windows
     if $::osfamily  == 'windows' {
       exec { 'set_dns':
-        command  => "set-DnsClientServerAddress -interfacealias Ethernet0 -serveraddresses ${adserver_ip[0][value]}, ${ip}",
-        unless   => "if ((Get-DnsClientServerAddress -addressfamily ipv4 -interfacealias Ethernet0).serveraddresses -notcontains '${adserver_ip[0][value]}'){exit 1}",
+        command  => "set-DnsClientServerAddress -interfacealias Ethernet0 -serveraddresses ${adserver_ip}, ${ip}",
+        unless   => "if ((Get-DnsClientServerAddress -addressfamily ipv4 -interfacealias Ethernet0).serveraddresses -notcontains '${adserver_ip}'){exit 1}",
         provider => powershell,
       }
     }
