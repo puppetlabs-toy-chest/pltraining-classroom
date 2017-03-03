4b9d90660fd7aae9fc3b45c8ffd2f22ed16decf8
Nekototori <jessykah.bird@gmail.com>
2017-03-03 14:10:52 -0600

    Using PQL to grab DNS ipaddress
    Since joining a domain requires the dns resolver to find the MS DNS records, this requires looking up the ipaddress of the AD DNS server, and then setting it as the DNS server.

--- manifests/windows/dns_server.pp
+++ manifests/windows/dns_server.pp
@@ -1,12 +1,13 @@
 # Set a DNS server on the windows agents
 define classroom::windows::dns_server (
+  $adserver_ip = 'facts[value]{name = "ipaddress" and certname = "adserver"},
   $ip = '8.8.8.8',
 ) {
     # Only run on windows
     if $::osfamily  == 'windows' {
       exec { 'set_dns':
-        command  => "set-DnsClientServerAddress -interfacealias Ethernet0 -serveraddresses ${ip}",
-        unless   => "if ((Get-DnsClientServerAddress -addressfamily ipv4 -interfacealias Ethernet0).serveraddresses -ne '${ip}'){exit 1}",
+        command  => "set-DnsClientServerAddress -interfacealias Ethernet0 -serveraddresses ${adserver_ip[0][value]}, ${ip}",
+        unless   => "if ((Get-DnsClientServerAddress -addressfamily ipv4 -interfacealias Ethernet0).serveraddresses -notcontains '${adserver_ip[0][value]}'){exit 1}",
         provider => powershell,
       }
     }
