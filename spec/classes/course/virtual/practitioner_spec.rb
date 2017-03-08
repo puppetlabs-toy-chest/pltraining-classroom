998edd7b8a54a8ff360d62b66f733d57aa6edf4a
Josh Samuelson <js@puppet.com>
2017-03-07 17:40:29 -0800

    Basic spec test for pracitioner virtual course

---
+++ spec/classes/course/virtual/practitioner_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe 'classroom::course::virtual::practitioner' do
+
+  parameter_matrix = [
+    { :offline => true },
+    { :offline => true, :control_owner => 'puppetlabs-education' },
+    { :control_owner => 'puppetlabs-education' }
+  ]
+  parameter_matrix.each do |params|
+    context "applied to master: #{params.to_s}" do
+      let(:pre_condition) {
+        "service { 'pe-puppetserver':
+          ensure => running,
+        }" + GLOBAL_PRE
+      }
+      let(:node) { 'master.puppetlabs.vm' }
+      let(:facts) { {
+        :servername => 'master.puppetlabs.vm'
+      } }
+      let(:params) { params }
+
+      it { should compile }
+    end
+
+    context "applied to agent: #{params.to_s}" do
+      let(:node) { 'agent.puppetlabs.vm' }
+      let(:facts) { {
+        :servername => 'master.puppetlabs.vm'
+      } }
+      let(:params) { params }
+
+      it { should compile }
+    end
+  end
+end
