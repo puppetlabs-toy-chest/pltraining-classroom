4fc52b831892d40eb36688063407514a7e4a8192
Josh Samuelson <js@puppet.com>
2017-04-05 14:44:46 -0700

    Create lens directories

--- manifests/agent/augeas.pp
+++ manifests/agent/augeas.pp
@@ -2,18 +2,22 @@ class classroom::agent::augeas {
   if $facts['augeas']['version'] == '1.4.0' {
     # The version of Augeas that ships with PE doesn't support current krb5.conf
     # until it's updated, install a new lens.
-    $lenses = [
-      '/opt/puppetlabs/puppet/share/augeas/lenses/dist/krb5.aug',
-      '/usr/share/augeas/lenses/dist/krb5.aug',
+    $lens_dirs = [
+      '/opt/puppetlabs/puppet/share/augeas/lenses/dist',
+      '/usr/share/augeas/lenses/dist',
     ]
-    
-    file { $lenses:
-      ensure => file,
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0644',
-      source => 'puppet:///modules/classroom/krb5.aug',
+
+    $lens_dirs.each |$lens_dir| {
+      file { $lens_dir:
+        ensure => directory,
+      }
+      file { "${lens_dir}/krb5.aug":
+        ensure => file,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+        source => 'puppet:///modules/classroom/krb5.aug',
+      }
     }
   }
-  
 }
