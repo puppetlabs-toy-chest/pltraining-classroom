5fb8f6e4570580c01c87d2b0de0ca9121a98c58a
Josh Samuelson <js@puppet.com>
2017-05-23 10:41:39 -0700

    Fix typo in windows dns server

--- manifests/windows/dns_server.pp
+++ manifests/windows/dns_server.pp
@@ -2,12 +2,11 @@
 define classroom::windows::dns_server (
   $ip = '8.8.8.8',
 ) {
-    }
     # Only run on windows
     if $::osfamily  == 'windows' {
       exec { 'set_dns':
         command  => "set-DnsClientServerAddress -interfacealias Ethernet0* -serveraddresses ${ip}",
-        unless   => "if ((Get-DnsClientServerAddress -addressfamily ipv4 -interfacealias Ethernet0*).serveraddresses -ne '${ip}'){exit 1}",
+        unless   => "if ((Get-DnsClientServerAddress -addressfamily ipv4 -interfacealias Ethernet0*).serveraddresses -ne '${ip}'){exit 1} else {exit 0}",
         provider => powershell,
       }
     }
