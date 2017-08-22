c71b56d52ed204622b7a88283c3af5183f653ed2
Ben Ford <ben.ford@puppetlabs.com>
2017-08-22 13:36:17 -0700

    update perf logging

--- manifests/master/perf_logging.pp
+++ manifests/master/perf_logging.pp
@@ -6,22 +6,10 @@ class classroom::master::perf_logging {
     ensure => present,
   }
 
-  package { 'aws-sdk':
-    ensure   => present,
-    provider => gem,
-  }
-
-  file { '/usr/local/bin/classroom_performance':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-    source => 'puppet:///modules/classroom/classroom_performance',
-  }
-
   cron { 'snapshot performance':
     ensure  => present,
-    command => '/usr/local/bin/classroom_performance snapshot',
+    command => 'classroom performance snapshot',
+    path    => '/bin:/usr/bin:/usr/local/bin',
     minute  => ['12', '42'],
   }
 
