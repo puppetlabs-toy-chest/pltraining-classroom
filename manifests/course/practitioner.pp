62618a26c4e8f1db1493b85252a30cca398c2758
Ben Ford <ben.ford@puppetlabs.com>
2018-02-12 11:42:24 -0800

    Switch to Virtual First
    This switches to virtual first delivery for our materials. The existing
    ILT courseware is moved to Legacy content. The Virtual courseware is
    promoted to being *the way* that we deliver courses, with two variants.
    
    The only difference between local/virtual deliveries right now is the
    one slide about introducing the virtual platform. But maintaining the
    two variants means that we can re-chunk as needed to meet the delivery
    requirements.
    
    This should be released on Wednesday or Thursday, so no trainings are
    affected.
    
    TRAINTECH-1563 #resolved #time 1h

--- manifests/course/practitioner.pp
+++ manifests/course/practitioner.pp
@@ -1,22 +1,52 @@
-# This is a wrapper class for the legacy config
-#
+# typing the parameters doesn't actually gain us anything, since the
+# Console doesn't provide any hinting. Subclasses validate types.
 class classroom::course::practitioner (
-  $offline            = undef,
-  $manage_yum         = undef,
-  $time_servers       = undef,
-  $jvm_tuning_profile = undef,
   $event_id           = undef,
   $event_pw           = undef,
+  $jvm_tuning_profile = $classroom::params::jvm_tuning_profile,
+  $offline            = $classroom::params::offline,
   $version            = undef,
-) {
-  # just wrap the classroom class
-  class { 'classroom_legacy::course::practitioner':
+) inherits classroom::params {
+  class { 'classroom::virtual':
     offline            => $offline,
-    manage_yum         => $manage_yum,
-    time_servers       => $time_servers,
     jvm_tuning_profile => $jvm_tuning_profile,
+    control_repo       => 'classroom-control-vp.git',
     event_id           => $event_id,
     event_pw           => $event_pw,
-    version            => $version,
   }
+
+  if $role == 'master' {
+    File {
+      owner => 'root',
+      group => 'root',
+      mode  => '0644',
+    }
+
+    include classroom::master::reporting_tools
+
+    class { 'classroom::facts':
+      coursename => 'practitioner',
+    }
+
+    class { 'classroom::master::showoff':
+      course             => 'Practitioner',
+      event_id           => $event_id,
+      event_pw           => $event_pw,
+      variant            => 'showoff',
+      version            => $version,
+    }
+
+  }
+  elsif $role == 'agent' {
+    puppet_enterprise::mcollective::client { 'peadmin':
+      activemq_brokers => ['master.puppetlabs.vm'],
+      keypair_name     => 'pe-internal-peadmin-mcollective-client',
+      create_user      => true,
+      logfile          => '/var/lib/peadmin/.mcollective.d/client.log',
+      stomp_password   => chomp(file('/etc/puppetlabs/mcollective/credentials','/dev/null')),
+      stomp_port       => 61613,
+      stomp_user       => 'mcollective',
+    }
+  }
+
 }
