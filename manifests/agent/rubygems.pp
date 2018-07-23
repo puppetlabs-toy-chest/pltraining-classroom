a00745117956b2df523f1f82ae66cc00da906d91
ipcrm <mcadorette@gmail.com>
2018-07-23 16:40:29 -0400

    (TRAINTECH-6701) Resolve ServerSpec Gem install issue for agent machines
    Prior to this commit the agent machines would try to install the latest
    serverspec and dependant specinfra, this cause lots of problems.
    Updating agent code to fix specinfra to 2.74.0 so that net-ssh 4.x would
    get intalled and we can continue using ruby 2.0.x (system Ruby)

--- manifests/agent/rubygems.pp
+++ manifests/agent/rubygems.pp
@@ -1,13 +1,22 @@
 class classroom::agent::rubygems {
   assert_private('This class should not be called directly')
 
-  package { [ 'rspec-puppet', 'puppetlabs_spec_helper' ]:
-    ensure   => present,
-    provider => 'puppet_gem',
+  # Required specinfra version to not require ruby >= 2.2.6
+  package { 'specinfra':
+    ensure   => '2.74.0',
+    provider => gem,
   }
 
   package { ['serverspec', 'rake']:
     ensure   => present,
     provider => 'gem',
+    require  => Package['specinfra'],
   }
+
+  package { [ 'rspec-puppet', 'puppetlabs_spec_helper' ]:
+    ensure   => present,
+    provider => 'puppet_gem',
+
+  }
+
 }
